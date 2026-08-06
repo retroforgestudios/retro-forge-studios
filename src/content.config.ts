212f4a58d49a2d27cc3859b2a0665b77b493db8f
John Gregory <johngregory@Johns-MacBook-Pro.local>
2026-08-06 18:19:57 -0500

    Connect schema entities and breadcrumbs

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
+    updated: z.coerce.date().optional(),
     author: z.string(),
     image: z.string().optional(),
     description: z.string(),
